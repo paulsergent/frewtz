b13f6f0b96c40f82ec61da454110fa4b998cb534
paulsergent <paulsergent.pro@gmail.com>
2025-04-19 08:58:09 +0100

    added farmer to product model

--- Frewtzapi/products/models.py
+++ Frewtzapi/products/models.py
@@ -4,6 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from farmers.models import Farmer
 
 
 # Create your models here.
@@ -14,11 +15,13 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.FloatField(default=0.0)
     stock = models.PositiveIntegerField(default=0)
-    thumbnail = models.ImageField(upload_to='products', blank=True, null=True)  
+    thumbnail = models.ImageField(upload_to='products', blank=True, null=True)
+    farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set the field to now when the object is first created.
     
 
     def __str__(self):  # This method returns the name of the product and creates a name-tag for this model
-        return f"{self.name} ({self.stock})"
+        return f"{self.name} ({self.stock}) from {self.farmer}"
     
     def get_absolute_url(self):
         return reverse('product', kwargs={'slug': self.slug})  # This method returns the URL of the product detail page based on the slug field. It uses the reverse() function to generate the URL dynamically. The reverse() function takes the name of the URL pattern and any arguments needed to construct the URL. In this case, it takes the slug field as an argument.
